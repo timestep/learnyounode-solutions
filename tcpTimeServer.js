6f26dfa09d99e14a8cae94c30149baec341cdacb
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-11-08 16:43:00 -0500

    TCP TIME

---
+++ tcpTimeServer.js
@@ -0,0 +1,21 @@
+(function () {
+  var net = require('net');
+  var moment = require('moment')
+  var cli_arguments = process.argv.slice(2);
+  var port = cli_arguments[0];
+
+  var currentTime = function () {
+    var date = moment().format('YYYY-MM-DD HH:mm');
+    return date + '\n';
+  }
+
+  var socketHandle = function (socket) {
+    socket.write(currentTime());
+    socket.end();
+  }
+
+  var server = net.createServer(socketHandle);
+  server.listen(port);
+
+
+})();
